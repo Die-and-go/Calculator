d52d14176ed478afc87afae39ed70c60fb1b567a
EvilR2D2 <diegoalaniz7575@gmail.com>
2023-03-20 21:44:25 -0500

    made the interface semi-functional, being able to console log the buttons clicked

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
-function add() {
-
-}
-
-function subtract() {
-
-}
-
-function multiply() {
-
-}
-
-function divide() {
-
-}
+//var operand1 = 1;
+//var operand2 = 2;
+//var operator = '+';
+//var exp = 1 + 2;
 
 //----------------------------//
 
-function operate() {
+//function add(a, b) {
+//    return a + b
+//}
 
+//function subtract(a, b) {
+//    return a - b 
+//}
+
+//function multiply(a, b) {
+//    return a * b
+//}
+
+//function divide(a, b) {
+//    return a / b
+//}
+
+//function op(a, b, x) {
+//    return x(a, b)
+//}
+
+//--------------------------//
+
+//let displayValue = '';
+
+//function writeDisplay() {
+//}
+
+function clickMe(button) {
+    let varName = button.value;
+
+    console.log(varName)
+
+    return varName
 }
 
